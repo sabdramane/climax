91b3261dd668fff6a2ab4494f3e28f6a32ab63ef
sanou <sanouabdramane5@gmail.com>
2021-06-01 12:19:22 +0000

    first commit

---
+++ src/test/java/com/agitex/climax/ClimaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agitex.climax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
